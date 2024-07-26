29daa3460663a240a02b2cc720f542e526c68fc0
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-07-26 23:22:04 +0530

    Completed template

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,9 +2,8 @@ module.exports = {
   // extends: ['@commitlint/config-conventional'],
   extends: [],
   rules: {
-    'header-min-length': [2, 'always', 20],
+    'header-min-length': [2, 'always', 10],
     'header-case-start-capital': [2, 'always'],
-    'header-end-period': [2, 'always'],
   },
   plugins: [
     {
@@ -14,9 +13,6 @@ module.exports = {
             /^[A-Z]/.test(raw),
             'Commit message must start with a capital letter',
           ]
-        },
-        'header-end-period': ({ header }) => {
-          return [/\.$/.test(header), 'Commit message must end with a period']
         },
       },
     },
